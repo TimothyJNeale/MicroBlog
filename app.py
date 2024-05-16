7f86845adde8e801717e4f86cc6087ae86a44ebf
TimothyJNeale <sixhillsai@icloud.com>
2024-05-16 17:16:42 +0100

    Formated date correctly

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+entries = []
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         entry_content = request.form['content']
         formated_date = datetime.datetime.today().strftime('%Y-%m-%d')
-        print(entry_content, formated_date)
-    return render_template('home.html') 
+        entries.append((entry_content, formated_date))
+        
+    entries_with_date = [
+        (
+            entry[0], 
+            entry[1], 
+            datetime.datetime.strptime(entry[1], '%Y-%m-%d').strftime('%b %d')
+        ) for entry in entries
+    ]
+    return render_template('home.html', entries=entries_with_date) 
