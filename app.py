1327c3544f405d1846a5a89e5721431c2fd971d5
TimothyJNeale <sixhillsai@icloud.com>
2024-05-20 20:03:51 +0100

    Use App factory pattern

--- app.py
+++ app.py
@@ -4,32 +4,35 @@ from pymongo import MongoClient
 
 import os
 
-database = os.getenv('MICROBLOG_DATABASE')
-username = os.getenv('MICROBLOG_USERNAME')
-password = os.getenv('MICROBLOG_PASSWORD')
+def create_app():
+    database = os.getenv('MICROBLOG_DATABASE')
+    username = os.getenv('MICROBLOG_USERNAME')
+    password = os.getenv('MICROBLOG_PASSWORD')
 
-connection_string = f"mongodb+srv://{username}:{password}@{database}.qvo1l4s.mongodb.net/"
+    connection_string = f"mongodb+srv://{username}:{password}@{database}.qvo1l4s.mongodb.net/"
 
-app = Flask(__name__)
-client = MongoClient(connection_string)
-app.db = client.microblog
+    app = Flask(__name__)
+    client = MongoClient(connection_string)
+    app.db = client.microblog
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        entry_content = request.form['content']
-        formated_date = datetime.datetime.today().strftime('%Y-%m-%d')
-        app.db.entries.insert_one({'content': entry_content, 'date': formated_date})
-        
+    @app.route('/', methods=['GET', 'POST'])
+    def home():
+        if request.method == 'POST':
+            entry_content = request.form['content']
+            formated_date = datetime.datetime.today().strftime('%Y-%m-%d')
+            app.db.entries.insert_one({'content': entry_content, 'date': formated_date})
+            
 
-    # Create list comprehension from entries to add formatted date
-    entries_with_date = [
-    (
-        entry['content'], 
-        entry['date'], 
-        datetime.datetime.strptime(entry['date'], '%Y-%m-%d').strftime('%b %d')
-    ) for entry in app.db.entries.find({})
-    ]
+        # Create list comprehension from entries to add formatted date
+        entries_with_date = [
+        (
+            entry['content'], 
+            entry['date'], 
+            datetime.datetime.strptime(entry['date'], '%Y-%m-%d').strftime('%b %d')
+        ) for entry in app.db.entries.find({})
+        ]
 
-    return render_template('home.html', entries=entries_with_date) 
+        return render_template('home.html', entries=entries_with_date)
+    
+    return app
